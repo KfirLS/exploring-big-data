5ac32cf7890a45db5f8566ecce290564da31a7f5
eliavs <eliavs@gmail.com>
2016-02-16 22:09:53 +0200

    again

--- midrag.py
+++ midrag.py
@@ -2,10 +2,13 @@
 import os
 import re
 from subprocess import call
-call(["wget","-r", "midrag.co.il"]) #take a very long time watch out
+#call(["wget","-r", "midrag.co.il"]) #take a very long time watch out
 a = os.listdir("midrag.co.il")
 for f in a:
     if "disSectorSpesific.asp@" in f:
         file = open("midrag.co.il/" + f, 'r')
         text = file.read()
-        print text[0:5]
+        ucd_text = unicode(text, 'windows-1255').strip()
+        all =  re.findall('OPTION.value=(.*)<\/',ucd_text)
+        for lin in all:
+            print lin + "\n"
