16c30af57a61364929fccd583987a48351134fae
eliavs <eliavs@gmail.com>
2016-02-17 22:50:35 +0200

    sort to functions

--- midrag.py
+++ midrag.py
@@ -2,14 +2,27 @@
 import os
 import re
 from subprocess import call
-#call(["wget","-r", "midrag.co.il"]) #take a very long time watch out
-a = os.listdir("midrag.co.il")
-for f in a:
-    if "disSectorSpesific.asp@" in f:
-        file = open("midrag.co.il/" + f, 'r')
-        text = file.read()
-        ucd_text = unicode(text, 'windows-1255').strip()
-        all =  re.finditer('OPTION.value=(\S*?)[\s>](.*?)<\/',ucd_text)
-        for a in all:
-            print a.group(1)
-            print a.group(2)
+
+
+def download_whole_site():
+    call(["wget","-r", "midrag.co.il"]) #take a very long time watch out
+
+
+def get_categories():
+    a = os.listdir("midrag.co.il")
+    for f in a:
+        if "disSectorSpesific.asp@" in f:
+            file = open("midrag.co.il/" + f, 'r')
+            text = file.read()
+            ucd_text = unicode(text, 'windows-1255').strip()
+            all =  re.finditer('OPTION.value=(\S*?)[\s>](.*?)<\/',ucd_text)
+            for a in all:
+                print a.group(1)
+                print a.group(2)
+                pass
+
+
+if __name__ == '__main__':
+    if "midrag.co.il" not in os.listdir(os.getcwd()):
+        download_whole_site()
+    get_categories()
